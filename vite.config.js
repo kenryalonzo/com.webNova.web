38618cb54c60c9e50f74c73b3ca202d0fc7f5be8
FOTIE FOTIE NATHANAEL <kenryalonzo@gmail.com>
2025-02-28 06:37:13 +0100

    feat: fonction printCV fonctionnelle

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss()],
+  build: {
+    cssCodeSplit: false, // Force la génération d'un seul fichier CSS
+  },
+  theme: {
+    extend: {
+      colors: {
+        beige: "#f5f5dc",
+      },
+    },
+  },
+});
